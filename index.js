0f3778d7bada71082ecac9dfce1178f8323b00b4
Dejon Kurti <50417797+dkurti@users.noreply.github.com>
2019-12-30 19:36:07 -0500

    3/3

--- index.js
+++ index.js
@@ -67,4 +67,93 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-const
+const server = http.createServer((req, res) => {
+/*     if (req.url === "/") {  //can also do //about to load about page
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),  //about.html 
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+
+    if (req.url === "/api/users") {
+        const users = [
+          { name: "Bob Smith", age: 40 },
+          { name: "John Doe", age: 30 }
+        ];
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(users));
+    } */
+
+// Build file path
+    let filePath = path.join(
+      __dirname,
+      "public",
+      req.url === "/" ? "index.html" : req.url
+    );
+
+    console.log(filePath);
+    res.end();
+
+// Extension of file
+    let extname = path.extnamen(filePath);
+
+// Initial Content Type
+    let contentType = 'text/html';
+
+// Check ext and set content type
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if(err.code == 'ENOENT') {
+                //Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) 
+                => {
+                    res.writeHead(200, { "Content-Type": "text/html" });      
+                    res.end(content, 'utf8');  
+                })
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content, 'utf8');  
+        }
+    });
+});
+
+
+  /* res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Homepage</h1>');  If you reload, doesn't change--have to manually restart server
+        Nodemon will continually watch this and update server w/ reload */
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
